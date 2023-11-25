10717916e0bf0e1f7974fed2affe260c80940dfd
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2023-11-25 20:48:34 +0300

    ParameterizedTest implementation in LoginITCase

--- src/test/java/ui/LoginITCase.java
+++ src/test/java/ui/LoginITCase.java
@@ -1,24 +1,31 @@
 package ui;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 import ui.Actions.PrepareDriver;
 import ui.Pages.MainPage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.WebDriver;
+
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class LoginITCase {
     static WebDriver driver;
     private final String URL = "https://fedchenkovyacheslav.github.io/";
-    private final String EMAIL = "g1@gmail.com";
-    private final String PASSWORD = "12345678";
-    private final String SIGN_IN = "form-sing-in";
     MainPage myMainPage;
 
-    @Before
-    public void setup(){
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("", "1", "This field is required"),
+                Arguments.of("1", "1", "Please enter a valid email address (your entry is not in the format \"somebody@example.com\")"),
+                Arguments.of("test@mail.com", "", "This field is required"),
+                Arguments.of("test@mail.com", "1", "This combination, mail and password were not found!")
+        );
+    }
+
+    @BeforeEach
+    public void setup() {
         driver = PrepareDriver.driverInit("chrome");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
@@ -26,34 +33,32 @@ public class LoginITCase {
         myMainPage = new MainPage(driver);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"g1@gmail.com, 12345678"})
     @DisplayName("Should login user")
-    public void loginLogoutToProfile(){
+    public void loginLogoutToProfile(String email, String password) {
         myMainPage
                 .clickOnSignIn()
-                .loginWithCredential(EMAIL, PASSWORD)
-                .checkValidMessagesInForm(SIGN_IN)
+                .loginWithCredential(email, password)
+                .checkValidMessagesInForm("form-sing-in")
                 .goToProfilePage()
                 .checkUrlIsValid(URL + "pages/profile/")
                 .clickOnSignOut()
                 .checkUrlIsValid(URL);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("testData")
     @DisplayName("Should check validation errors in login popup")
-    public void checkValidationErrorsOnLogin(){
+    public void checkValidationErrorsOnLogin(String email, String password, String validationError) {
         myMainPage
                 .clickOnSignIn()
-                .checkErrorInLoginForm("This field is required")
-                .typeEmail("1")
-                .checkErrorInLoginForm("Please enter a valid email address (your entry is not in the format \"somebody@example.com\")")
-                .typeEmail("test@mail.com")
-                .typePassword("1")
-                .checkErrorInLoginForm("This combination, mail and password were not found!");
+                .loginWithCredential(email, password)
+                .checkErrorInLoginForm(validationError);
     }
 
-    @After
-    public void quit(){
+    @AfterEach
+    public void quit() {
         driver.quit();
     }
 }
