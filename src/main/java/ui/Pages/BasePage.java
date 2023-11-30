49d8870b9a09e72a838c1901ae290922f2680eae
Viacheslav Fedchenko <viacheslav.fedchenko@piano.io>
2023-11-30 12:17:44 +0300

    fix

--- src/main/java/ui/Pages/BasePage.java
+++ src/main/java/ui/Pages/BasePage.java
@@ -14,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class BasePage {
+public abstract class BasePage {
     protected final WebDriver driver;
     static SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
 
